ac13fb0e47803d6455f4439981d6e0ed035f3c8e
Franklin-TECHZAR <103103061+Franklin-TECHZAR@users.noreply.github.com>
2024-10-03 15:08:34 +0530

    cron time changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('jobs:job-auto-expiry-date')->everyMinute();
-        $schedule->command('user:update-user-login-status')->everyMinute();
+        $schedule->command('jobs:job-auto-expiry-date')->everyFiveMinutes();
+        $schedule->command('user:update-user-login-status')->everyFiveMinutes();
         $schedule->command('create-opening-balance')->dailyAt('23:55'); // 11:55PM
     }
 
